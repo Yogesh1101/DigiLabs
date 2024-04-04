01507d92af1b01a15ceb544dfb730bd3b2495585
Yogesh1101 <jryogesh1121@gmail.com>
2024-04-04 11:50:47 +0530

    Final Update

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
+
+import withPWA from "next-pwa";
+
+withPWA({
+  dest: "public",
+});
+
 const nextConfig = {};
 
-export default nextConfig;
+export default withPWA(nextConfig);
